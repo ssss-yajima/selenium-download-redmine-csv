e60a5a2e9bc09d23ddb63617d105f61fdcdb98d9
y_megane <y.megane88@gmail.com>
2021-08-03 02:40:21 +0900

    add selenium saple programs

---
+++ src/selenium_sample.py
@@ -0,0 +1,14 @@
+import time
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+driver = webdriver.Chrome(options=options)
+
+driver.get('https://www.google.com/')
+time.sleep(5)
+search_box = driver.find_element_by_name("q")
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+time.sleep(5)
+driver.quit()
